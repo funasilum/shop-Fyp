177cf8b7efd7b5d19713c38c856e1e9ae792983d
usman <usmanakhtar426@gmail.com>
2023-05-06 07:56:26 +0500

    routes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://shop-fyp.vercel.app/',
   credentials: true
 }));
 
